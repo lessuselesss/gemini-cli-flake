c08871770b9fcecfae0b3ef79ef99c5f00cecc6b
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-06-25 21:36:35 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,27 +9,24 @@
     imports = [ flake-parts.flakeModules.easyOverlay ];
     systems = lib.systems.flakeExposed;
 
-    perSystem = { config, pkgs, ... }: {
-      overlayAttrs = {
-        gemini-cli = config.packages.gemini-cli;
-      }       
-       packages.gemini-cli = pkgs.buildNpmPackage {
-         pname = "gemini";
-            version = "0.0.1";
-            src = pkgs.fetchFromGitHub {
-              owner = "google-gemini";
-              repo = "gemini-cli";
-              rev = "0915bf7d677504c28b079693a0fe1c853adc456e";
-              hash = "sha256-s1K3bNEDdqy2iz3bk/3RdaCoGDenfc6knARzO/q3YcE=";
-            };
-            npmDepsHash = "sha256-2zyMrVykKtN+1ePQko9MVhm79p7Xbo9q0+r/P22buQA=";
-            npmPackFlags = [ "--ignore-scripts" ];
-            dontCheckForBrokenSymlinks = true;
-          };
-        
-          defaultPackage = config.packages.gemini-cli; # <-- Add this line
-        };
+   perSystem = { config, pkgs, ... }: {
+  overlayAttrs = {
+    gemini-cli = config.packages.gemini-cli;
+  };
+  packages = {
+    gemini-cli = pkgs.buildNpmPackage {
+      pname = "gemini";
+      version = "0.0.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "google-gemini";
+        repo = "gemini-cli";
+        rev = "0915bf7d677504c28b079693a0fe1c853adc456e";
+        hash = "sha256-s1K3bNEDdqy2iz3bk/3RdaCoGDenfc6knARzO/q3YcE=";
       };
+      npmDepsHash = "sha256-2zyMrVykKtN+1ePQko9MVhm79p7Xbo9q0+r/P22buQA=";
+      npmPackFlags = [ "--ignore-scripts" ];
+      dontCheckForBrokenSymlinks = true;
     };
-  });
-}
+    defaultPackage = config.packages.gemini-cli;
+  };
+};
